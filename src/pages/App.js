b8cbdda0a4144f52baa222646d8e6bb68df80b89
joaquineichholz <jeichholz3@uc.cl>
2021-07-06 18:41:44 -0400

    fix homepage path 3

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,17 @@
 import './../css/App.css';
 import Login from './Login';
 import Sheet from './Sheet';
-import { Route } from "react-router-dom";
-import { HashRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter } from "react-router-dom";
 
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Route exact path="/"component={Login}/>
       <Route exact path="/home"component={Sheet}/>
     </div>
-    </Router>
+    </BrowserRouter>
 
   );
 }
